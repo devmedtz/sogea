634bb3f0033c80ef2018dcf271433272c08134ff
medson <medsonnaftal@gmail.com>
2021-03-03 22:10:53 +0300

    sgdsgdhsgd

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 from posts.models import Post
@@ -7,14 +7,14 @@ from posts.models import Post
 def homepage(request):
 
     posts = Post.objects.all().order_by('-created_at')
-    featured = Post.objects.get(featured=True)
+    featured_list = get_object_or_404(Post, featured=True)
     common_tags = Post.tags.most_common()[:4]
 
     template_name = 'main/index.html'
 
     context = {
         'posts':posts,
-        'featured':featured,
+        'featured_list':featured_list,
         'common_tags':common_tags,
     }
 
