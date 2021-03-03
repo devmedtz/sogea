76edcae0e356a87144611bc723944584bd8103d3
medson <medsonnaftal@gmail.com>
2021-03-03 22:58:43 +0300

    shdjshdjshd

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'), name='main'),
+    path('', include('main.urls', namespace='main')),
     path('accounts/', include('allauth.urls')),
     # path('user/', include('accounts.urls', namespace='accounts'))
 
