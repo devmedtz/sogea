298d3da2d20a211bae29014c4566435829a188e3
medson <medsonnaftal@gmail.com>
2021-03-03 23:45:45 +0300

    sdshdhsgd

--- main/views.py
+++ main/views.py
@@ -6,15 +6,15 @@ from posts.models import Post
 def homepage(request):
 
     posts = Post.objects.all().order_by('-created_at')
-    featured = get_object_or_404(Post, featured=True)
-    common_tags = Post.tags.most_common()[:4]
+    #featured = get_object_or_404(Post, featured=True)
+    #common_tags = Post.tags.most_common()[:4]
 
     template_name = 'main/index.html'
 
     context = {
         'posts':posts,
-        'featured':featured,
-        'common_tags':common_tags,
+        #'featured':featured,
+        #'common_tags':common_tags,
     }
 
     return render(request, template_name, context=context)
