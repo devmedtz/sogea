9eb424626e5960df14d3a522f3303e566a0e907c
medson <medsonnaftal@gmail.com>
2021-03-11 16:49:39 +0300

    sdgshgdhsgdhs

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
+from allauth.account.signals import user_signed_up
+from allauth.socialaccount.models import SocialAccount
+
 from .models import Profile
 
 User = get_user_model
@@ -10,3 +13,35 @@ User = get_user_model
 def post_save_create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+
+@receiver(user_signed_up) 
+def populate_profile(sociallogin, user, **kwargs):    
+
+    if sociallogin.account.provider == 'facebook':
+        user_data = user.socialaccount_set.filter(provider='facebook')[0].extra_data
+        picture_url = "http://graph.facebook.com/" + sociallogin.account.uid + "/picture?type=large"
+
+
+    if sociallogin.account.provider == 'linkedin':
+        user_data = user.socialaccount_set.filter(provider='linkedin')[0].extra_data        
+        picture_url = user_data['picture-urls']['picture-url']
+        print('picture_url:',picture_url)
+
+    if sociallogin.account.provider == 'google':
+        user_data = SocialAccount.objects.filter(provider='google', user_id=self.user.id)
+        picture_url = "not available"
+        if len(user_data):
+            picture_url = user_data[0].extra_data['picture']
+        print('picture_url:',picture_url)
+
+    # if sociallogin.account.provider == 'twitter':
+    #     user_data = user.socialaccount_set.filter(provider='twitter')[0].extra_data
+    #     picture_url = user_data['profile_image_url']
+    #     picture_url = picture_url.rsplit("_", 1)[0] + "." + picture_url.rsplit(".", 1)[1]
+    #     email = user_data['email']
+    #     first_name = user_data['name'].split()[0]
+
+    user.profile.profile_picture = picture_url
+    user.profile.save()  
