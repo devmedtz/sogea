fb24671de9e808504bcaf754680e705481bc4946
medson <medsonnaftal@gmail.com>
2021-03-17 18:25:20 +0300

    sdsjhdjshdjs

--- marketing/views.py
+++ marketing/views.py
@@ -5,32 +5,6 @@ from django.shortcuts import render
 from .forms import EmailSignupForm
 from .models import EmailSignup
 
-import json
-import requests
-
-MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
-MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
-MAILCHIMP_EMAIL_LIST_ID = settings.MAILCHIMP_EMAIL_LIST_ID
-
-api_url = 'https://{dc}.api.mailchimp.com/3.0'.format(dc=MAILCHIMP_DATA_CENTER)
-members_endpoint = '{api_url}/lists/{list_id}/members'.format(
-    api_url=api_url,
-    list_id=MAILCHIMP_EMAIL_LIST_ID
-)
-
-
-def subscribe(email):
-    data = {
-        "email_address": email,
-        "status": "subscribed"
-    }
-    r = requests.post(
-        members_endpoint,
-        auth=("", MAILCHIMP_API_KEY),
-        data=json.dumps(data)
-    )
-    return r.status_code, r.json()
-
 
 def email_list_signup(request):
     form = EmailSignupForm(request.POST or None)
@@ -40,6 +14,6 @@ def email_list_signup(request):
             if email_signup_qs.exists():
                 messages.info(request, "You are already subscribed")
             else:
-                subscribe(form.instance.email)
                 form.save()
+                messages.success(request, "You are successfully subscribed")
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
