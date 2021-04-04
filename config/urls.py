a993555b51b3f9ef1e7183b2818b4dbabf3b726f
medson <medsonnaftal@gmail.com>
2021-04-05 01:13:44 +0300

    dsjdhsjdh

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from ckeditor_uploader.views import upload
 
 
 urlpatterns = [
@@ -13,8 +15,11 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
     path('marketing/', include('marketing.urls', namespace='marketing')),
     path('admin/', include('admins.urls', namespace='admins')),
+
+    path('ckeditor/upload/', login_required(upload), name='ckeditor_upload'),
     
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
     path('comment/', include('comment.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
